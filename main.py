361666efe25a21b2196143b53e88c25ed3259fc0
AvikSarkar0 <92179341+AvikSarkar0@users.noreply.github.com>
2024-09-23 17:53:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+# import streamlit as st
+# import pyjokes
+# import pyttsx3
+
+
+
+# # text = st.text_input('Enter some text')
+# if st.button('Hit me'):
+#   joke = pyjokes.get_joke()
+#   st.text(f"{joke}")
+#   engine = pyttsx3.init()
+#   engine.say(joke)
+#   engine.runAndWait()
+
+
 import streamlit as st
 import pyjokes
-import pyttsx3
+from gtts import gTTS
+import os
 
-
-
-# text = st.text_input('Enter some text')
 if st.button('Hit me'):
-  joke = pyjokes.get_joke()
-  st.text(f"{joke}")
-  engine = pyttsx3.init()
-  engine.say(joke)
-  engine.runAndWait()
+    joke = pyjokes.get_joke()
+    st.text(f"{joke}")
+
+    # Convert joke to speech using gTTS
+    tts = gTTS(joke)
+    tts.save('joke.mp3')
+    os.system('mpg123 joke.mp3')  # Play the saved MP3 file
