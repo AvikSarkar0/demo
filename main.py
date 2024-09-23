a7ade294e24660fe27d334e7d6d82299e6a8c11b
AvikSarkar0 <92179341+AvikSarkar0@users.noreply.github.com>
2024-09-23 17:50:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pyjokes
+import pyttsx3
+
 
 
 # text = st.text_input('Enter some text')
 if st.button('Hit me'):
   joke = pyjokes.get_joke()
   st.text(f"{joke}")
-
-
+  engine = pyttsx3.init()
+  engine.say(joke)
+  engine.runAndWait()
